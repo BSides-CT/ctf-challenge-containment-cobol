310358878563ed7d69dc71ed91c633baffc0ed8b
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-09-03 08:32:11 -0400

    Start adding some visual content

--- containment_unit.cbl
+++ containment_unit.cbl
@@ -3,9 +3,18 @@
          *> setup the program id
          PROGRAM-ID. CONTAINMENTUNIT.
          *> setup the procedure division (equiv. main() function)
+
+       DATA DIVISION.
+         *> Set variables
+         WORKING-STORAGE SECTION.
+
+         01 TCID50RELPFU PIC S9(1)V9(2) VALUE 0.69.
+ 
+
        PROCEDURE DIVISION.
          *> print welcome 
          DISPLAY 'COVID-2020 Containment Unit'.
          DISPLAY 'Danger Biohazard'.
+         DISPLAY 'Relationship between TCID(50) and PFU: 'TCID50RELPFU.
        *> end program
        STOP RUN.
