1d1e8972ef49b1207db4845a32c75a87a1523ff5
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-09-03 08:22:13 -0400

    Add initial program file

---
+++ containment_unit.cbl
@@ -0,0 +1,11 @@
+       *> setup the identification division
+       IDENTIFICATION DIVISION.
+         *> setup the program id
+         PROGRAM-ID. CONTAINMENTUNIT.
+         *> setup the procedure division (equiv. main() function)
+       PROCEDURE DIVISION.
+         *> print welcome 
+         DISPLAY 'COVID-2020 Containment Unit'.
+         DISPLAY 'Danger Biohazard'.
+       *> end program
+       STOP RUN.
