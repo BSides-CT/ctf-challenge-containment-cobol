3491907d4008b85b4935571aaf877ea520947e03
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-09-03 09:24:16 -0400

    Add biohazard symbol

--- containment_unit.cbl
+++ containment_unit.cbl
@@ -9,15 +9,31 @@
          WORKING-STORAGE SECTION.
 
          01 TCID50RELPFU PIC S9(1)V9(2) VALUE 0.69.
- 
 
        PROCEDURE DIVISION.
-       MAIN-PROCEDUE.
-         *> print welcome 
-         DISPLAY 'COVID-2020 Containment Unit'.
-         DISPLAY 'Danger Biohazard'.
+       MAIN-PROCEDURE.
+         *> print system welcome message (move to screen section) 
+
+         DISPLAY "*********************************".
+         DISPLAY "* COVID-2020 Containment Unit   *".
+         DISPLAY "* Developed in 1986             *".
+         DISPLAY "* By the retro*causal engineers *".
+         DISPLAY "*********************************".
+         DISPLAY "                                 ".
+         DISPLAY "        .*_; ;_*.                ".
+         DISPLAY "       / /     \ \               ".
+         DISPLAY "      | |       | |              ". 
+         DISPLAY "       \ \.***./ /               ".
+         DISPLAY "   .*'z   .***.   z'*.           ".
+         DISPLAY " ,`.*z/ .'`***`'. \z*.`,         ".
+         DISPLAY " '`   | | \(_)/ | |   `'         ".
+         DISPLAY " ,    \  \ | | /  /    ,         ".
+         DISPLAY " ;`'.,_\  `*'*'  /_,.'`;         ".
+         DISPLAY "  '*._  _.*'^'*._  _.*'          ".
+         DISPLAY "      ``         ``              ".
+         DISPLAY "     Danger Biohazard            ".
 
        PFU-PROCEDURE.
-         DISPLAY 'Relationship between TCID(50) and PFU: 'TCID50RELPFU.
+         DISPLAY "Relationship between TCID(50) and PFU: "TCID50RELPFU.
        *> end program
        STOP RUN.
