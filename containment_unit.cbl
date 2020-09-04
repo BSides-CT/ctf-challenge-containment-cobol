d601696e922196d3c353f86d468e9320ca018da3
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-09-04 17:06:16 -0400

    Add a quick update + screenshot

--- containment_unit.cbl
+++ containment_unit.cbl
@@ -21,6 +21,8 @@
 
          WORKING-STORAGE SECTION.
 
+         01 WS-MENU               PIC X     VALUE "M".
+
          01 TCID50RELPFU PIC S9(1)V9(2) VALUE 0.69.
 
          01 RESPONSE-INTRO.
@@ -28,11 +30,11 @@
 
          *> Login credentials when C pressed
          01 RESPONSEC.
-            05 RESPONSE-IN-WS  PIC X     VALUE "C".
-            05 LOGGED-IN       PIC X     VALUE "F".
+            05 RESPONSE-IN-WS  PIC X        VALUE "C".
+            05 LOGGED-IN       PIC X        VALUE "F".
          01 DATA-FROM-SCREEN.
-            05 ID-IN-WS        PIC XXXX  VALUE SPACES.  
-            05 PWD-IN-WS       PIC X(20) VALUE SPACES.  
+            05 ID-IN-WS        PIC XXXX     VALUE SPACES.  
+            05 PWD-IN-WS       PIC X(20)    VALUE SPACES.  
 
          *> User data read in from file
          01 WS-USER.
@@ -43,6 +45,12 @@
          *> Menu item selection
          01 MENURESPONSE.
             05 RESPONSE-IN-MENU PIC X    VALUE "X".
+
+         *> Settings screen
+         01 SETTINGSVALUES.
+            05 ACCOUNT-EXPIRATION PIC X(6) VALUE "123120".
+         01 SETTINGSRESPONSE.
+            05 RESPONSE-IN-SETTINGS PIC X VALUE "X".
 
          SCREEN SECTION.
 
@@ -61,29 +69,29 @@
          05 VALUE "                                 "
                          LINE 6 COL 1.
          05 VALUE "        .*_; ;_*.                "
-                         LINE 7 COL 1.
+                         FOREGROUND-COLOR 2 LINE 7 COL 1.
          05 VALUE "       / /     \ \               "
-                         LINE 8 COL 1.
+                         FOREGROUND-COLOR 2 LINE 8 COL 1.
          05 VALUE "      | |       | |              " 
-                         LINE 9 COL 1.
+                         FOREGROUND-COLOR 2 LINE 9 COL 1.
          05 VALUE "       \ \.***./ /               "
-                         LINE 10 COL 1.
+                         FOREGROUND-COLOR 2 LINE 10 COL 1.
          05 VALUE "   .*'z   .***.   z'*.           "
-                         LINE 11 COL 1.
+                         FOREGROUND-COLOR 2 LINE 11 COL 1.
          05 VALUE " ,`.*z/ .'`***`'. \z*.`,         "
-                         LINE 12 COL 1.
+                         FOREGROUND-COLOR 2 LINE 12 COL 1.
          05 VALUE " '`   | | \(_)/ | |   `'         "
-                         LINE 13 COL 1.
+                         FOREGROUND-COLOR 2 LINE 13 COL 1.
          05 VALUE " ,    \  \ | | /  /    ,         "
-                         LINE 14 COL 1.
+                         FOREGROUND-COLOR 2 LINE 14 COL 1.
          05 VALUE " ;`'.,_\  `*'*'  /_,.'`;         "
-                         LINE 15 COL 1.
+                         FOREGROUND-COLOR 2 LINE 15 COL 1.
          05 VALUE "  '*._  _.*'^'*._  _.*'          "
-                         LINE 16 COL 1.
+                         FOREGROUND-COLOR 2 LINE 16 COL 1.
          05 VALUE "      ``         ``              "
-                         LINE 17 COL 1.
+                         FOREGROUND-COLOR 2 LINE 17 COL 1.
          05 VALUE "     Danger Biohazard            "
-                         LINE 18 COL 1.
+                         FOREGROUND-COLOR 4 LINE 18 COL 1.
          05 VALUE "     Press L to Login:           "
                          LINE 19 COL 1.
          05  RESPONSE-INPUT                             LINE 19 COL 23
@@ -92,7 +100,7 @@
          01  DATA-ENTRY-SCREEN.
          05  VALUE "DATA ENTRY SCREEN" BLANK SCREEN     LINE 1 COL 35.
          05  VALUE "ID #"                               LINE 3 COL 10.
-         05   ID-INPUT                                  LINE 3 COL 25
+         05  ID-INPUT                                   LINE 3 COL 25
                          PIC  X(4)     TO ID-IN-WS.
          05  VALUE "PASSWORD"                           LINE 5 COL 10.
          05  NAME-INPUT                                 LINE 5 COL 25
@@ -112,14 +120,28 @@
                                                 LINE 5 COL 10.
          05  VALUE "CONTAINMENT (U)NIT - access the unit"   
                                                 LINE 7 COL 10.
+         05  VALUE "(Q)UIT - exit the program"   
+                                                LINE 9 COL 10.
          05  VALUE "Please select an option:"                
                                                 LINE 11 COL 10.
          05  RESPONSE-INPUT                     LINE 11 COL 34
                          PIC X         TO RESPONSE-IN-MENU.  
 
+         01 SETTINGS-SCREEN.
+         05 VALUE "SETTINGS SCREEN"
+                        BLANK SCREEN            LINE 1 COL 35.
+         05 VALUE "User ID:"                    LINE 2 COL 10.
+         05 ID-OUTPUT PIC XXXX FROM ID-IN-WS    LINE 2 COL 25. 
+         05 VALUE "Change password:"            LINE 3 COL 10.
+         05 RESPONSE-INPUT
+                        PIC X          TO RESPONSE-IN-SETTINGS.
+         05 VALUE "Account expiration:"         LINE 4 COL 10. 
+         05 ACC-EXPIRE-OUTPUT 
+            PIC X(6) FROM ACCOUNT-EXPIRATION    LINE 4 COL 30. 
+
+
        PROCEDURE DIVISION.
        *> print system welcome message
-
 
        OPEN INPUT USERS.
           PERFORM UNTIL WS-EOF='Y'
@@ -128,7 +150,6 @@
              END-READ
           END-PERFORM.
        CLOSE USERS. 
-
 
        PERFORM UNTIL RESPONSE-IN-LOGIN = "L"
           DISPLAY INTRO-SCREEN
@@ -150,12 +171,19 @@
           ELSE 
              DISPLAY "Login Error"
           END-IF
-
        END-PERFORM.
 
-
-       DISPLAY MAIN-MENU-SCREEN. 
-       ACCEPT  MAIN-MENU-SCREEN.
+       PERFORM UNTIL WS-MENU = "Q"
+          EVALUATE WS-MENU
+            WHEN "M" DISPLAY MAIN-MENU-SCREEN
+                     ACCEPT  MAIN-MENU-SCREEN
+                     MOVE RESPONSE-IN-MENU TO WS-MENU
+            WHEN "S" DISPLAY SETTINGS-SCREEN
+                     ACCEPT  SETTINGS-SCREEN   
+                     MOVE "M" TO WS-MENU
+            WHEN other MOVE "M" TO WS-MENU
+          END-EVALUATE
+       END-PERFORM. 
 
        *>PFU-PROCEDURE.
        *>  DISPLAY "Relationship between TCID(50) and PFU: "TCID50RELPFU.
