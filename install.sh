71490700cc112c01428985f745c66e6a577d345c
rpigu-i <8628078+rpigu-i@users.noreply.github.com>
2020-09-12 08:34:26 -0400

    Update install instructions

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 export TERM=xterm
 if [[ ${ENV} == "DEV" ]]; then 
    cobc -x -o containment_unit containment_unit.cbl
+   /etc/init.d/xinetd restart
 else
    cobc -x -o containment_unit containment_unit.cbl
    rm containment_unit.cbl
+   /etc/init.d/xinetd restart
 fi
 
